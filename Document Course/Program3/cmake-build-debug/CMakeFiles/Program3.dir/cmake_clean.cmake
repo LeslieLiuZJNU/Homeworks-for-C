07243a44eec2baef8fec02f3b1bcf991d08cb01c
思久方为远见 <3321924836@qq.com>
2020-11-04 19:37:15 +0800

    Update

---
+++ Document Course/Program3/cmake-build-debug/CMakeFiles/Program3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Program3.dir/main.cpp.obj"
+  "Program3.exe"
+  "Program3.exe.manifest"
+  "Program3.pdb"
+  "libProgram3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Program3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Document Course/Program3/main.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int x[10];
+    int y[10];
+    for(int i=0;i<10;i++)scanf("%d%d",&x[i],&y[i]);
+
+    return 0;
+}
